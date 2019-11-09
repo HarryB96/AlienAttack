109f3be00a35278522f12f281d0509a0cd2a567f
HarryB96 <57000678+HarryB96@users.noreply.github.com>
2019-11-09 11:55:12 +0000

    Added player movement and shooting controls

--- AlienAttack/AlienAttack/Form1.Designer.cs
+++ AlienAttack/AlienAttack/Form1.Designer.cs
@@ -29,12 +29,45 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Red;
+            this.player.Location = new System.Drawing.Point(375, 500);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- AlienAttack/AlienAttack/Form1.cs
+++ AlienAttack/AlienAttack/Form1.cs
@@ -12,9 +12,80 @@ namespace AlienAttack
 {
     public partial class Form1 : Form
     {
+        bool goLeft;
+        bool goRight;
+        bool isPressed;
+        int playerSpeed = 10;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Z && !isPressed)
+            {
+                isPressed = true;
+                makeBullet();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (isPressed)
+            {
+                isPressed = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goLeft && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight && player.Left < 730)
+            {
+                player.Left += playerSpeed;
+            }
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox && (y.Tag == "bullet"))
+                {
+                    y.Top -= 20;
+                }
+                if (((PictureBox)y).Top < this.Height - 600)
+                {
+                    this.Controls.Remove(y);
+                }
+            }
+        }
+
+        private void makeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Size = new Size(5, 5);
+            bullet.Tag = "bullet";
+            bullet.BackColor = System.Drawing.Color.Black;
+            bullet.Left = player.Left + player.Width / 2;
+            bullet.Top = player.Top - 5;
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
         }
     }
 }
