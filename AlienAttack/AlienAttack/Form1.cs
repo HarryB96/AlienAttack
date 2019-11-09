2f1778201a3aa08facb28892dbaa2f57436c5d9d
HarryB96 <57000678+HarryB96@users.noreply.github.com>
2019-11-09 20:03:26 +0000

    Made enemies spawn and disappear when hit by a bullet

--- AlienAttack/AlienAttack/Form1.cs
+++ AlienAttack/AlienAttack/Form1.cs
@@ -16,9 +16,16 @@ namespace AlienAttack
         bool goRight;
         bool isPressed;
         int playerSpeed = 10;
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            if (timer1.Enabled)
+            {
+                makeBasicEnemies(6);
+                makeMediumEnemies(0);
+            }
         }
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
@@ -74,6 +81,24 @@ namespace AlienAttack
                     this.Controls.Remove(y);
                 }
             }
+
+            foreach (Control i in this.Controls)
+            {
+                foreach (Control j in this.Controls)
+                {
+                    if (i is PictureBox && i.Tag == "basic enemy")
+                    {
+                        if (j is PictureBox && j.Tag == "bullet")
+                        {
+                            if (i.Bounds.IntersectsWith(j.Bounds))
+                            {
+                                this.Controls.Remove(i);
+                                this.Controls.Remove(j);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void makeBullet()
@@ -86,6 +111,47 @@ namespace AlienAttack
             bullet.Top = player.Top - 5;
             this.Controls.Add(bullet);
             bullet.BringToFront();
+        }
+
+        private void makeBasicEnemies(int number)
+        {
+            int i = 0;
+            while (i < number)
+            {
+                PictureBox basic = new PictureBox();
+                basic.Size = new Size(50, 50);
+                basic.Tag = "basic enemy";
+                basic.BackColor = System.Drawing.Color.Blue;
+                int x = rnd.Next(0, 16);
+                int y = rnd.Next(0, 40);
+                basic.Left = x * 50;
+                basic.Top = y * 10;
+                this.Controls.Add(basic);
+                basic.BringToFront();
+                i++;
+            }
+
+        }
+        private void makeMediumEnemies(int number)
+        {
+            int i = 0;
+            while (i < number)
+            {
+                PictureBox medium = new PictureBox();
+                medium.Size = new Size(100, 50);
+                medium.Tag = "medium enemy";
+                medium.BackColor = System.Drawing.Color.Yellow;
+                medium.Left = rnd.Next(0, 730);
+                medium.Top = rnd.Next(0, 400);
+                this.Controls.Add(medium);
+                medium.BringToFront();
+                i++;
+            }
+
+        }
+        private void gameOver()
+        {
+            timer1.Stop();
         }
     }
 }
